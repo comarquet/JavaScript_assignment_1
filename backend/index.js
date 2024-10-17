efa61525c9a9f3bd0d2f3f91c5e2bbb49863552a
Quentin <qrichaud@gmail.com>
2024-10-17 19:08:18 +0200

    Setup backend server

---
+++ backend/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const { program } = require('commander');
+const { query, body, matchedData, validationResult } = require('express-validator');
+
+
+/**
+ * Prepare command line arguments
+ */
+program
+    .option('-p, --port <PORT>', 'Select port on which the server will be run', 8000);
+program.parse();
+let port = program.opts().port;
+
+/** Setup Express server */
+const app = express();
+app.use(express.json());
+
+function resetChat() {
+    return [{author: "admin", message: "Welcome to the chat!"}]; 
+}
+
+/**
+ * Message format : 
+ * { 
+ *      author: "name",
+ *      message: "content"
+ * }
+ */
+let chat = resetChat();
+
+app.get('/chat', (req, res) => {
+    res.json(chat);
+})
+
+/**
+ * Expected JSON body : 
+ * {
+ *      "author": "name",
+ *      "message": "content"
+ * }
+ */
+app.post(
+    '/message', 
+    body('author').exists().trim().isString().notEmpty(),
+    body('message').exists().trim().isString().notEmpty(),
+    (req, res) => {
+        const validationRes = validationResult(req);
+        if (validationRes.isEmpty()) {
+            chat.push(matchedData(req));
+            res.json(chat);
+        } else {
+            res.status(400).json({errors: validationRes.array()})
+        }
+    }
+)
+
+/**
+ * Expected querystring parameter "message".
+ * 
+ * List of censored words : Lyon, Paris, England, English
+ * 
+ * Response : JSON object.
+ * 
+ * {
+ *    "originalMessage": "The English are a great people",
+ *    "censoredMessage": "The *** are a great people"
+ * }
+ */
+app.get('/censorMessage', query('message').exists(), (req, res) => {
+    const validationRes = validationResult(req);
+    if (validationRes.isEmpty()) {
+        let originalMessage = matchedData(req).message;
+        let censoredMessage = originalMessage.replaceAll(/lyon|paris|england|english/gi, '***');
+        res.json({originalMessage, censoredMessage});
+    } else {
+        res.status(400).json({errors: validationRes.array()})
+    }
+});
+
+
+/**
+ * Clears the chat. Returns the new cleared chat
+ */
+app.delete('/chat', (req, res) => {
+    chat = resetChat();
+    res.json(chat);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
